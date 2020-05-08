5d3f95845bfaddeef19b0264abc2aa695a032761
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-05-08 12:53:33 +0900

    Added PlayerRating to the tests.

--- inhouse_bot/tests/test_sqlite.py
+++ inhouse_bot/tests/test_sqlite.py
@@ -2,6 +2,7 @@ import discord
 import pytest
 
 from inhouse_bot.common_utils import discord_token
+from inhouse_bot.sqlite.player_rating import PlayerRating
 from inhouse_bot.sqlite.sqlite_utils import get_session
 from inhouse_bot.sqlite.player import Player
 
@@ -12,7 +13,8 @@ client = discord.Client()
 @pytest.mark.asyncio
 async def test_player():
     """
-    Simply tests the addition of the Bot user as a player itself. Requires it to not be in the db yet.
+    Tests the addition of the Bot user as a player itself.
+    Also tests PlayerRating and makes sure the on_cascade flags are applied properly.
     """
     await client.login(discord_token)
 
@@ -20,12 +22,20 @@ async def test_player():
 
     player = Player(test_user)
 
-    session.add(player)
+    player = session.merge(player)
     session.commit()
 
-    assert player == session.query(Player).filter(Player.discord_id == 707853630681907253).one()
+    assert player == session.query(Player).filter(Player.discord_id == test_user.id).one()
+
+    player_rating = PlayerRating(player, 'mid')
+
+    session.add(player_rating)
+    session.commit()
+
+    assert player.ratings
 
     session.delete(player)
     session.commit()
 
     assert session.query(Player).filter(Player.discord_id == test_user.id).one_or_none() is None
+    assert session.query(PlayerRating).filter(PlayerRating.player_id == test_user.id).one_or_none() is None
