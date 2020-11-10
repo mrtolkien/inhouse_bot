10c5e21980a6aea8c5282610cc640765580bf2cb
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-11-10 14:17:22 +0900

    Removing obsolete old files + minor field name changes

--- bot_orm/tables/game.py
+++ bot_orm/tables/game.py
@@ -26,7 +26,7 @@ class Game(bot_declarative_base):
     id = Column(Integer, primary_key=True)
 
     # Game creation date
-    date = Column(DateTime)
+    start = Column(DateTime)
 
     # Server the game was played from
     server_id = Column(BigInteger)
@@ -66,7 +66,7 @@ class Game(bot_declarative_base):
                     for (team, role) in sorted(self.participants, key=lambda x: roles_list.index(x[1]))
                     if team == team_column
                 ]
-                for team_column in ["blue", "red"]
+                for team_column in side_enum
             },
             headers="keys",
         )
@@ -82,46 +82,14 @@ class Game(bot_declarative_base):
         from bot_orm.tables.game_participant import GameParticipant
         from matchmaking_logic.evaluate_game import evaluate_game
 
-        self.date = datetime.datetime.now()
+        self.start = datetime.datetime.now()
 
         # First, we write down the participants
         self.participants = {
             (team, role): GameParticipant(team, role, players[team, role]) for team, role in players
         }
 
+        self.server_id = list(self.participants.values())[0].player_server_id
+
         # Then, we compute the expected blue side winrate (which we use for matchmaking)
         self.blue_expected_winrate = evaluate_game(self)
-
-    def update_trueskill(self):
-        """
-        Updates the game’s participants TrueSkill values based on the game result.
-        """
-        import trueskill
-
-        session = object_session(self)
-
-        # TODO Rewrite that so it’s more readable
-
-        # participant.trueskill represents pre-game values
-        # p.player.ratings[p.role] is the PlayerRating relevant to the game that was scored
-        team_ratings = {
-            side: {
-                p.player.ratings[p.role]: trueskill.Rating(p.trueskill_mu, p.trueskill_sigma)
-                for p in self.participants.values()
-                if p.side == side
-            }
-            for side in ["BLUE", "RED"]
-        }
-
-        if self.winner == "BLUE":
-            new_ratings = trueskill.rate([team_ratings["BLUE"], team_ratings["RED"]])
-        else:
-            new_ratings = trueskill.rate([team_ratings["RED"], team_ratings["BLUE"]])
-
-        for team in new_ratings:
-            for player_rating in team:
-                player_rating.trueskill_mu = team[player_rating].mu
-                player_rating.trueskill_sigma = team[player_rating].sigma
-                session.add(player_rating)
-
-        session.commit()
