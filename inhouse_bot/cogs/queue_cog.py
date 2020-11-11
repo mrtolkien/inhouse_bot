46c9c6f13bc3a88feed043e508e34f269e47a157
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-11-11 16:43:37 +0900

    New emoji code that works with no emoji setup

--- inhouse_bot/cogs/queue_cog.py
+++ inhouse_bot/cogs/queue_cog.py
@@ -11,6 +11,7 @@ from inhouse_bot.config.emoji import get_role_emoji
 
 from inhouse_bot import game_queue
 from inhouse_bot import matchmaking_logic
+from inhouse_bot.game_queue import GameQueue
 
 from inhouse_bot.inhouse_bot import InhouseBot
 
@@ -19,33 +20,34 @@ class QueueCog(commands.Cog, name="Queue"):
     def __init__(self, bot: InhouseBot):
         self.bot = bot
 
+        # This should be a table
         self.latest_queue_messages = {}
 
-    async def send_queue(self, ctx: commands.Context, queue: game_queue.GameQueue):
+    async def send_queue(self, ctx: commands.Context):
         """
         Deletes the previous queue message and sends a new one in the channel
         """
+        channel_id = ctx.channel.id
+
         try:
-            old_queue_message = self.latest_queue_messages[ctx.channel.id]
+            old_queue_message = self.latest_queue_messages[channel_id]
         except KeyError:
             old_queue_message = None
 
         rows = []
 
         # Creating the queue visualisation requires getting the Player objects from the DB to have the names
-        with session_scope() as session:
-            queue_players = game_queue.get_queue_players(queue, session)
+        queue = GameQueue(channel_id)
 
-            # We need to check all roles to still look good even with empty queues
-            for role in roles_list:
-                rows.append(f"{get_role_emoji(role)} " + ", ".join(p.name for p in queue_players[role]))
+        for role, role_queue in queue.queue_players_dict.items():
+            rows.append(f"{get_role_emoji(role)} " + ", ".join(qp.player.name for qp in role_queue))
 
         # Create the queue embed
         embed = Embed(colour=embeds_color)
         embed.add_field(name="Queue", value="\n".join(rows))
 
         # We save the message object in our local cache
-        self.latest_queue_messages[ctx.channel.id] = await ctx.send(embed=embed)
+        self.latest_queue_messages[channel_id] = await ctx.send(embed=embed)
 
         # Sequenced that way for smoother scrolling in discord
         if old_queue_message:
@@ -68,7 +70,7 @@ class QueueCog(commands.Cog, name="Queue"):
         """
 
         # Actually queuing the player
-        queue = game_queue.add_player(
+        game_queue.add_player(
             player_id=ctx.author.id,
             name=ctx.author.name,
             role=role,
@@ -76,23 +78,22 @@ class QueueCog(commands.Cog, name="Queue"):
             server_id=ctx.guild.id,
         )
 
-        if game := matchmaking_logic.find_best_game(queue):
+        if game := matchmaking_logic.find_best_game(GameQueue(ctx.channel.id)):
             score = abs(0.5 - game.blue_expected_winrate)
 
-            if score < 0.1:
-                # Good situation where we have a relatively fair game
-                ...
             if score < 0.2:
-                # Acceptable situation where we will still start the game
+                # Good situation where we have a relatively fair game
+                # TODO Create game, start ready check, drop players, ...
                 ...
             else:
                 # One side has over 70% predicted winrate, we do not start
                 await ctx.send(
-                    f"The best match found had a side with a {.5 + score} predicted winrate and therefore did not start"
+                    f"The best match found had a side with a {.5 + score}%"
+                    f" predicted winrate and therefore did not start"
                 )
 
         # Currently, we only update the current queue even if other queues got changed
-        await self.send_queue(ctx=ctx, queue=queue)
+        await self.send_queue(ctx=ctx)
 
     @commands.command(aliases=["leave", "stop"])
     @guild_only()
@@ -106,10 +107,10 @@ class QueueCog(commands.Cog, name="Queue"):
             !stop_queue
         """
 
-        queue = game_queue.remove_player(player_id=ctx.author.id, channel_id=ctx.channel.id)
+        game_queue.remove_player(player_id=ctx.author.id, channel_id=ctx.channel.id)
 
         # Currently, we only update the current queue even if other queues got changed
-        await self.send_queue(ctx=ctx, queue=queue)
+        await self.send_queue(ctx=ctx)
 
     @commands.command(aliases=["win", "wins", "victory"])
     @guild_only()
@@ -153,4 +154,4 @@ class QueueCog(commands.Cog, name="Queue"):
 
             # TODO Display the game and tag the players to vote and cancel the game (*ie* delete it from DB)
 
-    # TODO Add admins functions (!reset, !cancel, !score)
+    # TODO Add admins functions (!reset, !cancel, !score, !mark_queue)
