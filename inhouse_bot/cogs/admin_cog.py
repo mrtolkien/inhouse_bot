475aef23499a866dd7e70f50fda153825e5e316e
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-11-12 11:09:01 +0900

    Better errors

--- inhouse_bot/cogs/admin_cog.py
+++ inhouse_bot/cogs/admin_cog.py
@@ -1,4 +1,3 @@
 # TODO Add admins functions, all starting with !admin
 #   !admin reset, accepting a channel or a user (if user, resets their queue status)
 #   !admin score, score a game for BLUE/RED
-#   !admin make_queue, makes the current channel a queue channel
