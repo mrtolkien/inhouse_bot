693197976028aaa1d9cfbb7a939cad8d7a446b56
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-11-14 10:32:39 +0900

    Test comment for future update

--- tests/matchmaking_logic/test_matchmaking_logic.py
+++ tests/matchmaking_logic/test_matchmaking_logic.py
@@ -47,11 +47,16 @@ def test_matchmaking_logic_priority():
     """
     game_queue.reset_queue()
 
+    # TODO LOW PRIO Rewrite the test to make it properly test age-based matchmaking, even with an empty DB
+    #   I think even without age-based matchmaking it could pass at the moment, since on an empty DB the first
+    #   tested game has a perfect 50% evaluation. Won’t happen after other tests though so ok atm
+
     # We queue for everything, with 0, 1, 2, 3 being top, 4, 5, 6, 7 being jgl, ...
     for player_id in range(0, 20):
         game_queue.add_player(player_id, roles_list[int(player_id / 4 % 5)], 0, 0)
 
     game = find_best_game(GameQueue(0))
+    print(game.blue_expected_winrate)
 
     # Assert we chose 0, 1, 4, 5, 8, 9, 13, 13, 16, 17 players
     for participant in game.participants.values():
