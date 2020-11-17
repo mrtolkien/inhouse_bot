02d82ecc01e5cf81b9a5279ea07fada2481fa395
jay209015 <jrivers209015@gmail.com>
2020-11-17 10:20:34 -0600

    Update admin_cog.py

--- inhouse_bot/cogs/admin_cog.py
+++ inhouse_bot/cogs/admin_cog.py
@@ -46,7 +46,7 @@ class AdminCog(commands.Cog, name="Admin"):
                 .filter(PlayerRating.player_id == ctx.author.id)
                 .filter(PlayerRating.role == role).first()
             )
-            if (!player_rating):
+            if isinstance(player_rating, PlayerRating) == false:
                 player_rating = PlayerRating(Player, role)
                 
             player_rating.trueskill_mu = mmr
